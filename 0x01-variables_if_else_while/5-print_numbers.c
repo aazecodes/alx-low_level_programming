d8d1fdd7ba38635006af98e735b262cab0950f1f
aazecodes <abdussalamsafiyah@gmail.com>
2023-04-22 08:11:46 -0700

    5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: prints from 0 to 9
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+
+	for (int digit = 0; digit <= 9; digit++)
+	{
+	printf("%d", digit);
+	}
+	printf("\n");
+	return (0);
+}
