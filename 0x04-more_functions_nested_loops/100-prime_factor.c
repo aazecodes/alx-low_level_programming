72aa007cba65a753e93dd2fae04e18b5ee7e07c8
aazecodes <abdussalamsafiyah@gmail.com>
2023-04-27 14:39:56 -0700

    100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -11,19 +11,19 @@ int main(void)
 	long number = 612852475143;
 	int fig;
 
-	for (fig = 1; fig < number / 2; fig++)
+	while (fig++ < number / 2)
 	{
 	if (number % fig == 0)
 	{
 	number /= 2;
 	continue;
 	}
+
 	for (fig = 3; fig < number / 2; fig += 2)
 	{
 	if (number % fig == 0)
-	{
 	number /= fig;
-	}
+
 	}
 	}
 	printf("%ld\n", number);
