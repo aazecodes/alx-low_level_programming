54832c395d72be9a876d3b32bb297a9d2622657a
aazecodes <abdussalamsafiyah@gmail.com>
2023-04-27 15:10:44 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,22 +11,23 @@ int main(void)
 	int num;
 
 	while (num++ < 100)
-
 	if ((num % 3 == 0) && (num % 5 == 0))
-		printf("FizzBuzz ");
+	printf("FizzBuzz ");
 
 	else if ((num % 3) == 0)
-		printf("Fizz ");
+	printf("Fizz ");
 
 	else if ((num % 5) == 0)
 	{
-		if (num != 100)
-		printf("Buzz ");
-		else
-			printf("Buzz ");
-	}
+	if (num != 100)
+	printf("Buzz ");
+
 	else
-		printf("%d ", num);
+	printf("Buzz");
+	}
+
+	else
+	printf("%d ", num);
 
 	printf("\n");
 	return (0);
