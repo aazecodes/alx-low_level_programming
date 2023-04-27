03bfe4982157fd22627377ea005a50d7038ca80c
aazecodes <abdussalamsafiyah@gmail.com>
2023-04-27 05:55:39 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*print_numbers - function that print all numbers from
+*0 to 9
+*
+*Return: returns nothing
+*/
+void print_numbers(void)
+{
+	int num = 48;
+
+	while (num <= 58)
+	{
+		_putchar(num);
+		(num++);
+	}
+		_putchar(10);
+}
