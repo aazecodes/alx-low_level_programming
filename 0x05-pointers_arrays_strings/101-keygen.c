e5ad6c922ddf760cb18db4d6bd45aeb9cc152605
aazecodes <abdussalamsafiyah@gmail.com>
2023-05-02 15:02:47 -0700

    101-keygen.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+/**
+ * main - program that generates random valid
+ * passwords for the program 101-crackme
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int idx;
+	char charset[] = "abcdefghijklmnopqrstuvwxyABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+	srand(time(NULL));
+	for (int i = 0; i < 100; i++)
+	{
+		idx = rand() % sizeof(charset) + 1;
+		_putchar(charset[idx]);
+	}
+	return (0);
+}
