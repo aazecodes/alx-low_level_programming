7275a2675529dd63ade3603f72886525b1bd4491
aazecodes <abdussalamsafiyah@gmail.com>
2023-05-02 13:57:18 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -20,12 +20,15 @@ int _atoi(char *s)
 			|| s[i] == '\n' || s[i] == '+' || s[i] == '-')
 	{
 		if (s[i] == '-')
-			sign = -1;
+			sign *= -1;
 		i++;
 	}
 	while (s[i] >= '0' && s[i] <= '9')
 	{
-		result = (result * 10) + (s[i] - '0');
+		int r = result * 10;
+		int q =  s[i] - '0';
+
+		result = r + q;
 		i++;
 	}
 	return (sign * result);
