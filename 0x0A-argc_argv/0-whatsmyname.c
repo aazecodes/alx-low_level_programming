1ac4c03177adb09e207f723af20e7d479802c48c
aazecodes <abdussalamsafiyah@gmail.com>
2023-05-12 11:54:51 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - program that prints its name, followed by a new line
+ * @argc: count of arguments
+ * @argv: vector array
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
