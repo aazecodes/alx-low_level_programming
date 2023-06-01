eb1245f5f05abc0d526e9e7184d2e70e9df59d43
aazecodes <abdussalamsafiyah@gmail.com>
2023-06-01 07:23:34 -0700

    onstart

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+ * onstart- Prints a string before the main function is executed
+ * Return: nothing
+ */
+void onstart(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+	"I bore my house upon my back!\n");
+}
