38687ae444e9faa246b05f4dafa16d085fe380d9
aazecodes <abdussalamsafiyah@gmail.com>
2023-07-07 11:54:07 -0700

    mandatory tasks on bit

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_binary - Prints binary
+ * @n: Unsigned int
+ * Return: Nothing
+ */
+void print_binary(unsigned long int n)
+{
+
+	if (n > 1)
+		print_binary(n >> 1);
+	(n & 1) ? _putchar('1') :  _putchar('0');
+
+}
